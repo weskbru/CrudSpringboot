9b5a64495c5293c80579ce206f2af98677bcc03b
Wesley <Wesley@DESKTOP-BH6M3VH>
2024-02-20 22:57:27 -0300

    iniciando projeto crud spring boot

---
+++ crud-springboot/src/main/java/br/com/crud_springboot/controllers/GreetingsController.java
@@ -0,0 +1,19 @@
+package br.com.crud_springboot.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GreetingsController {
+  
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+        return "Projeto spring " + name + "!";
+    }
+}
